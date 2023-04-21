63160f947f2b34406a9dea731cc8d9cbfd7fd92f
Caio Abdon <caio.alves54@etec.sp.gov.br>
2023-04-21 18:15:35 -0300

    feat(getId): busca de usuarios por ID no db

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -11,3 +11,15 @@ exports.getAllUsers = (offset, limit) => {
         });
     });
 };
+
+exports.getUserById = (id) => {
+    return new Promise((resolve, reject) => {
+        const sql = `SELECT * FROM users WHERE id = ?`;
+        db.get(sql, [id], (err, row) => {
+            if (err) {
+                reject(err);
+            }
+            resolve(row);
+        });
+    });
+};
