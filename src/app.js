9adc88f13bc189b6cdf154b7b1508438975ace19
Caio Abdon <caio.alves54@etec.sp.gov.br>
2023-04-23 02:47:56 -0300

    fix(bug): a palavra require estava escrita errada

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
-const cors = requrie('@koa/cors');
+const cors = require('@koa/cors');
 const userRouter = require('./routes/userRoutes');
 
 const app = new Koa();
