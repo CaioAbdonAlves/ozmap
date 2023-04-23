30dbd8aa6a76916c81229d37b62935b1dd1ba394
Caio Abdon <caio.alves54@etec.sp.gov.br>
2023-04-23 02:44:19 -0300

    refactor(export): exportando o server para usar nos testes

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const app = require('./app');
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`O servidor está rodando na porta: ${PORT}`);
 });
+
+module.exports = server;
